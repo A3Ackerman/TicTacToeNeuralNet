665bf972ec2a8fb5f9aa83bba72eab858b856b4d
Alex <>
2021-01-25 12:30:48 -0800

    framework coming together

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,2 +1,20 @@
-def hi():
-    return "lol"
+import numpy as np
+
+
+WEIGHTS = np.random.random((3, 10))
+LEARNING_RATE_INITIAL = 0.01
+LEARNING_RATE_MINIMUM = 0.0001
+
+
+def sweepForward():
+    pass
+
+
+def backPropogate():
+    pass
+
+
+def train(epoch: int):
+    for _ in range(epoch):
+        sweepForward()
+        backPropogate()
