86454b7c6c636c94619db85874d99230a9da38aa
Alex <>
2021-02-07 21:50:41 -0800

    react working

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <p>Welcome to the Tic Tac Toe Neural Net! This project is currently under development.</p>
+    <p>This is a Neural Net consisting of a single fully connected layer (no hidden layers) with a sigmoid activation function and cross-entropy loss</p>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
