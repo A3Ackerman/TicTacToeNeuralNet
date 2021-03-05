05f4b8424b83bde49f2f01dcc2605d461e5a1395
Alex <>
2021-03-04 21:14:21 -0800

    front end nearly finished

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import React, {useState, useEffect} from 'react'
 /* React code below based on https://reactjs.org/tutorial/tutorial.html */
 
 const dec = 4
+let train_data = require('./train_data.json');
+let test_data = require('./test_data.json');
+
 
 declare global {
 
@@ -24,41 +27,59 @@ declare global {
   }
 }
 
-function Square() {
-  const [val, setVal] = useState(-1.000)
+function Intro() {
+  return (
+    <div>
+      <h2>Welcome to the Tic Tac Toe Neural Net!</h2>
+        <p>The networks consists of a single fully connected layer with a sigmoid activation function and cross-entropy loss function</p>
+      <p>The goal of this project is to train a simple neural net to classify complete games of Tic Tac Toe as "X-Wins", "O-Wins", or "Draw".</p>
+      <p>To expand the problem space beyond the finite number of possible 3x3 Tic Tac Toe boards, Xs and Os are represented by random values in the range (0, 0.5) and (0.5, 1) respectively.</p>
+  </div>
+  )
+}
+
+function Square({i, onClick}: {i: number, onClick: Function}) {
   const [dis, setDis] = useState(false)
 
   return (
     <button className="square" 
       disabled={dis} 
-      onClick={() => {setVal(Math.random()); setDis(true)}}
-      style={val < 0 ? {background: "#fff"} : val > 0.5 ? {background: "#e4b5b5"} : {background: "#99b1f4"}}
+      onClick={() => {onClick(); setDis(true)}}
+      style={i == null ? {background: "#fff"} : i > 0.5 ? {background: "#e4b5b5"} : {background: "#99b1f4"}}
       >
-      {val.toFixed(dec)}
+      {i ? i.toFixed(dec): i}
     </button>
   )
 }
 
 function Board() {
-  const status = 'Next player: X';
+  const [xNext, setXNext] = useState(true)
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick(i: number){
+    let sq: number[] = squares
+    sq[i] = xNext ? Math.random()/2 : 0.5 + Math.random()/2
+    setSquares(sq)
+    setXNext(!xNext)
+  }
 
   return (
-    <div>
-      <div className="status">{status}</div>
+    <div className="game-board">
+      <div className="status">{xNext ? "Next player: X": "Next player: O"}</div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square i={squares[0]} onClick={() => handleClick(0)}/>
+        <Square i={squares[1]} onClick={() => handleClick(1)}/>
+        <Square i={squares[2]} onClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square i={squares[3]} onClick={() => handleClick(3)}/>
+        <Square i={squares[4]} onClick={() => handleClick(4)}/>
+        <Square i={squares[5]} onClick={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square i={squares[6]} onClick={() => handleClick(6)}/>
+        <Square i={squares[7]} onClick={() => handleClick(7)}/>
+        <Square i={squares[8]} onClick={() => handleClick(8)}/>
       </div>
     </div>
   );
@@ -67,9 +88,7 @@ function Board() {
 function Game(){
   return (
     <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
+      <Board /> 
       <div className="game-info">
         <div>{/* status */}</div>
         <ol>{/* TODO */}</ol>
@@ -87,32 +106,35 @@ function TrainingSquare(i: number) {
   );
 }
 
-type boardArray = [[number, number, number], [number, number, number], [number, number, number]]
+type boardArray = [number, number, number, number, number, number, number, number, number]
 
 function TrainingBoard(board: boardArray) {
 
   return (
     <div className='training-board'>
       <div className="board-row">
-        {TrainingSquare(board[0][0])}
-        {TrainingSquare(board[0][1])}
-        {TrainingSquare(board[0][2])}
+        {TrainingSquare(board[0])}
+        {TrainingSquare(board[1])}
+        {TrainingSquare(board[2])}
       </div>
       <div className="board-row">
-        {TrainingSquare(board[1][0])}
-        {TrainingSquare(board[1][1])}
-        {TrainingSquare(board[1][2])}
+        {TrainingSquare(board[3])}
+        {TrainingSquare(board[4])}
+        {TrainingSquare(board[5])}
       </div>
       <div className="board-row">
-        {TrainingSquare(board[2][0])}
-        {TrainingSquare(board[2][1])}
-        {TrainingSquare(board[2][2])}
+        {TrainingSquare(board[6])}
+        {TrainingSquare(board[7])}
+        {TrainingSquare(board[8])}
       </div>
     </div>
   );
 }
 
-function ShowTrainingBoards({title, boards}: {title: string, boards: boardArray[]}) {
+
+type json_board = {game: boardArray, class: number}
+
+function ShowTrainingBoards({title, boards}: {title: string, boards: json_board[]}) {
 
   return (
     <table className='training-boards'>
@@ -125,7 +147,7 @@ function ShowTrainingBoards({title, boards}: {title: string, boards: boardArray[
         </tr>
       </thead>
       <tbody>
-        {boards.map(x => <tr><td headers="board">{TrainingBoard(x)}</td><td headers='label'>X Wins</td><td headers='pred'>Draw</td></tr>)} 
+        {boards.map((x, i) => <tr key={i}><td headers="board">{TrainingBoard(x.game)}</td><td headers='label'>{x.class}</td><td headers='pred'>None</td></tr>)} 
       </tbody>
     </table>
   )
@@ -144,11 +166,11 @@ function Table({weights, bias} : tableProps) {
   let dataRows: JSX.Element[][] = []
 
   weights.forEach(
-    row => {
+    (row, i) => {
       let cells: JSX.Element[] = []
-      row.forEach(val => {
+      row.forEach((val, j) => {
         cells.push(
-          <td headers='weights'>{val.toFixed(dec)}</td>
+          <td headers='weights' key={i.toString().concat(j.toString())}>{val.toFixed(dec)}</td>
         )
       })
       dataRows.push(cells)
@@ -168,9 +190,9 @@ function Table({weights, bias} : tableProps) {
           </tr>
         </thead>
         <tbody>
-          <tr><th>Class X Wins</th><td headers='bias'>{b0}</td>{dataRows[0]}</tr>
-          <tr><th>Class Y Wins</th><td headers='bias'>{b1}</td>{dataRows[1]}</tr>
-          <tr><th>Class Draw</th><td headers='bias'>{b2}</td>{dataRows[2]}</tr>
+          <tr key="0r"><th key="0h">Class X Wins</th><td headers='bias' key="0b">{b0}</td>{dataRows[0]}</tr>
+          <tr key="1r"><th key="1h">Class Y Wins</th><td headers='bias' key='1b'>{b1}</td>{dataRows[1]}</tr>
+          <tr key="2r"><th key="2h">Class Draw</th><td headers='bias' key='2b'>{b2}</td>{dataRows[2]}</tr>
         </tbody>
       </table>
     </div>
@@ -193,7 +215,7 @@ function App() {
   const [weights, setWeights] = useState([[],[],[]] as number[][])
   const [bias, setBias] = useState([] as string[])
 
-  let boards: boardArray[] = [[[0.1,0.2,0.6], [0.1,0.2,0.3], [0.1,0.2,0.3]],[[0.1,0.2,0.3], [0.1,0.2,0.3], [0.1,0.2,0.3]]]
+
 
   //Initialize Python
   useEffect(() => {
@@ -218,16 +240,17 @@ function App() {
       }
     }, [pythonLoaded])
 
+
   return (
-    <div className="App">        
-        <div className='python-console'>{console}</div>
-        <Game />
-        <Table weights={weights} bias={bias}/>
-        <div>
-          <ShowTrainingBoards title={"Training Dataset"} boards={boards}/>
-          <ShowTrainingBoards title={"Test Dataset"} boards={boards}/>
-        </div>
-        
+    <div className="App">
+      <Intro />     
+      <div className='python-console'>{console}</div>
+      <Game />
+      <Table weights={weights} bias={bias}/>
+      <div>
+        <ShowTrainingBoards title={"Training Dataset"} boards={train_data}/>
+        <ShowTrainingBoards title={"Test Dataset"} boards={test_data}/>
+      </div>
     </div>
   
   )
--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <p>Welcome to the Tic Tac Toe Neural Net! This project is currently under development.</p>
-    <p>This is a Neural Net consisting of a single fully connected layer (no hidden layers) with a sigmoid activation function and cross-entropy loss</p>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
