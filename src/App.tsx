078f38f52a99a8dfecf4c45e118c9c9e10f1dc90
Alex <>
2021-03-12 17:54:06 -0800

    automated generaion of training/test data

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import React, {useState, useEffect} from 'react'
 /* React code below based on https://reactjs.org/tutorial/tutorial.html */
 
 const dec = 4
-let train_data = require('./train_data.json');
-let test_data = require('./test_data.json');
 
+type boardVal = number | null
+type boardArray = [boardVal, boardVal, boardVal, boardVal, boardVal, boardVal, boardVal, boardVal, boardVal]
 
 declare global {
 
@@ -27,114 +27,149 @@ declare global {
   }
 }
 
+
+const gameStatus = {
+  XWIN: "X Wins!",
+  OWIN: "O Wins!",
+  DRAW: "Draw!",
+  XN: "Next Player: X",
+  ON: "Next Player: 0"
+}
+
 function Intro() {
   return (
     <div>
       <h2>Welcome to the Tic Tac Toe Neural Net!</h2>
-        <p>The networks consists of a single fully connected layer with a sigmoid activation function and cross-entropy loss function</p>
-      <p>The goal of this project is to train a simple neural net to classify complete games of Tic Tac Toe as "X-Wins", "O-Wins", or "Draw".</p>
-      <p>To expand the problem space beyond the finite number of possible 3x3 Tic Tac Toe boards, Xs and Os are represented by random values in the range (0, 0.5) and (0.5, 1) respectively.</p>
+        <p>The networks consists of a single fully connected layer with a sigmoid activation function and cross-entropy loss function.</p>
+      <p>The goal of this project is to train a simple neural net to classify complete games of Tic Tac Toe as "X-Wins", "O-Wins", or "Draw", under the convention that X always plays first.</p>
+      <p>To expand the problem space beyond the finite number of possible 3x3 Tic Tac Toe boards, Xs and Os are represented by random values in the range <span style={{backgroundColor: "#99b1f4"}}>(0, 0.5)</span> and <span style={{backgroundColor:  "#e4b5b5"}}>(0.5, 1)</span> respectively.</p>
   </div>
   )
 }
 
-function Square({i, onClick}: {i: number, onClick: Function}) {
-  const [dis, setDis] = useState(false)
+function getGameStatus(squares: boardArray) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if(!squares[a] || !squares[b] || !squares[c]){
+      continue  // skip loop if any of the cells in the line are empty
+    }
+
+    if (squares[a] < 0.5 && squares[b] < 0.5 && squares[c] < 0.5){
+      return gameStatus.XWIN // Class X wins
+    } else if (squares[a] >= 0.5 && squares[b] >= 0.5 && squares[c] >= 0.5) {
+      return gameStatus.OWIN; // Class Y wins
+    }
+  }
+  if(squares.includes(null)){
+    if(squares.filter(v => v === null).length % 2){
+      return gameStatus.XN
+    } else {
+      return gameStatus.ON
+    }
+  } else {
+    return gameStatus.DRAW // Class Draw
+  }
+}
+
+function Square(props: any) {
 
   return (
     <button className="square" 
-      disabled={dis} 
-      onClick={() => {onClick(); setDis(true)}}
-      style={i == null ? {background: "#fff"} : i > 0.5 ? {background: "#e4b5b5"} : {background: "#99b1f4"}}
+      disabled={props.i !== null || props.disabled} 
+      onClick={props.onClick}
+      style={props.i == null ? {background: "#fff"} : props.i > 0.5 ? {background: "#e4b5b5"} : {background: "#99b1f4"}}
       >
-      {i ? i.toFixed(dec): i}
+      {props.i ? props.i.toFixed(dec): props.i}
     </button>
   )
 }
 
-function Board() {
-  const [xNext, setXNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
+function Board(props: any) {
+
+  const [squares, setSquares] = useState(Array(9).fill(null) as boardArray)
+  const [status, setStatus] = useState(gameStatus.XN)
 
   function handleClick(i: number){
-    let sq: number[] = squares
-    sq[i] = xNext ? Math.random()/2 : 0.5 + Math.random()/2
+    let sq: boardArray = squares.slice() as boardArray
+    sq[i] = status === gameStatus.XN ? Math.random()/2 : 0.5 + Math.random()/2
+    let st = getGameStatus(sq)
+    // update state, but won't take effect until after render
     setSquares(sq)
-    setXNext(!xNext)
+    setStatus(st)
+  }
+
+  function saveAndReset(){
+    props.saveBoard(squares)
+    setSquares(Array(9).fill(null) as boardArray)
+    setStatus(gameStatus.XN)
   }
 
   return (
-    <div className="game-board">
-      <div className="status">{xNext ? "Next player: X": "Next player: O"}</div>
-      <div className="board-row">
-        <Square i={squares[0]} onClick={() => handleClick(0)}/>
-        <Square i={squares[1]} onClick={() => handleClick(1)}/>
-        <Square i={squares[2]} onClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square i={squares[3]} onClick={() => handleClick(3)}/>
-        <Square i={squares[4]} onClick={() => handleClick(4)}/>
-        <Square i={squares[5]} onClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square i={squares[6]} onClick={() => handleClick(6)}/>
-        <Square i={squares[7]} onClick={() => handleClick(7)}/>
-        <Square i={squares[8]} onClick={() => handleClick(8)}/>
-      </div>
-    </div>
-  );
-}
-
-function Game(){
-  return (
     <div className="game">
-      <Board /> 
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+      <div className="game-board">
+        <div className="status">{status}</div>
+        <div className="board-row">
+          <Square i={squares[0]} onClick={() => handleClick(0)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+          <Square i={squares[1]} onClick={() => handleClick(1)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+          <Square i={squares[2]} onClick={() => handleClick(2)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+        </div>
+        <div className="board-row">
+          <Square i={squares[3]} onClick={() => handleClick(3)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+          <Square i={squares[4]} onClick={() => handleClick(4)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+          <Square i={squares[5]} onClick={() => handleClick(5)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+        </div>
+        <div className="board-row">
+          <Square i={squares[6]} onClick={() => handleClick(6)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+          <Square i={squares[7]} onClick={() => handleClick(7)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+          <Square i={squares[8]} onClick={() => handleClick(8)} disabled={!(status === gameStatus.XN || status === gameStatus.ON)}/>
+        </div>
       </div>
+      <button onClick={() => saveAndReset()} disabled={status === gameStatus.XN || status === gameStatus.ON}>Add game to Test Dataset</button>
     </div>
   );
 }
 
-function TrainingSquare(i: number) {
-
-  return (
-    <button className="training-square" style={i > 0.5 ? {background: "#e4b5b5"} : {background: "#99b1f4"}}>
-      {i.toFixed(dec)}
-    </button>
-  );
-}
-
-type boardArray = [number, number, number, number, number, number, number, number, number]
 
 function TrainingBoard(board: boardArray) {
 
   return (
     <div className='training-board'>
       <div className="board-row">
-        {TrainingSquare(board[0])}
-        {TrainingSquare(board[1])}
-        {TrainingSquare(board[2])}
+        <Square i={board[0]}/>
+        <Square i={board[1]}/>
+        <Square i={board[2]}/>
       </div>
       <div className="board-row">
-        {TrainingSquare(board[3])}
-        {TrainingSquare(board[4])}
-        {TrainingSquare(board[5])}
+        <Square i={board[3]}/>
+        <Square i={board[4]}/>
+        <Square i={board[5]}/>
       </div>
       <div className="board-row">
-        {TrainingSquare(board[6])}
-        {TrainingSquare(board[7])}
-        {TrainingSquare(board[8])}
+        <Square i={board[6]}/>
+        <Square i={board[7]}/>
+        <Square i={board[8]}/>
       </div>
     </div>
   );
 }
 
 
-type json_board = {game: boardArray, class: number}
+function ShowTrainingBoards({title, boards}: {title: string, boards: boardArray[]}) {
 
-function ShowTrainingBoards({title, boards}: {title: string, boards: json_board[]}) {
+  function classifyGame(g: boardArray){
+    window.pyodide.runPythonAsync()
+  }
 
   return (
     <table className='training-boards'>
@@ -147,7 +182,7 @@ function ShowTrainingBoards({title, boards}: {title: string, boards: json_board[
         </tr>
       </thead>
       <tbody>
-        {boards.map((x, i) => <tr key={i}><td headers="board">{TrainingBoard(x.game)}</td><td headers='label'>{x.class}</td><td headers='pred'>None</td></tr>)} 
+        {boards.map((b, i) => <tr key={i}><td headers="board">{TrainingBoard(b)}</td><td headers='label'>{getGameStatus(b)}</td><td headers='pred'>{}</td></tr>)} 
       </tbody>
     </table>
   )
@@ -199,6 +234,23 @@ function Table({weights, bias} : tableProps) {
     )
 }
 
+function RunPythonWithParam(props: any){
+  const [N, SetN] = useState(props.default)
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    props.onSubmit(props.pythonCall + "(" + N + ")")
+    e.preventDefault()
+  }
+
+  return(
+  <form onSubmit={e => handleSubmit(e)}>
+    <label htmlFor="quantity">{props.label}</label>
+    <input type="text" id="trainN" name="trainN" min="1" max="100" onChange={e => SetN(e.target.value)} value={N}/>
+    <input type="submit" value={props.buttonText}/>
+  </form>
+  )
+}
+
 async function loadNeuralNetScripts()  {
   let response = await fetch('./NeuralNet.py')
   let mlcode = await response.text()
@@ -214,7 +266,8 @@ function App() {
   const [pythonLoaded, setPythonLoaded] = useState(false)
   const [weights, setWeights] = useState([[],[],[]] as number[][])
   const [bias, setBias] = useState([] as string[])
-
+  const [trainData, setTrainData] = useState([])
+  const [testData, setTestData] = useState([])
 
 
   //Initialize Python
@@ -240,16 +293,45 @@ function App() {
       }
     }, [pythonLoaded])
 
+    function saveBoard(board: boardArray){
+      let cp = testData.slice()
+      cp.push(board)
+      setTestData(cp)
+    }
+
+    function generateTrainingGames(code: string){
+      window.pyodide.runPythonAsync(code)
+        .then((output: boardArray[])=> {
+            setTrainData(trainData.concat(output))
+        })
+        .catch((err: string) => {
+            setConsole(console + err + '\n')
+        });
+    }
+
+    function generateTestGames(code: string){
+      window.pyodide.runPythonAsync(code)
+        .then((output: boardArray[])=> {
+            setTestData(testData.concat(output))
+        })
+        .catch((err: string) => {
+            setConsole(console + err + '\n')
+        });
+    }
 
   return (
     <div className="App">
       <Intro />     
       <div className='python-console'>{console}</div>
-      <Game />
+      <RunPythonWithParam label="N = " buttonText="Generate N Training Games" default="50" pythonCall="generate_n_games" onSubmit={(e: string) => generateTrainingGames(e)}/>
+      <RunPythonWithParam label="N =  " buttonText="Generate N Test Games" default="20" pythonCall="generate_n_games" onSubmit={(e: string) => generateTestGames(e)}/>
+      <RunPythonWithParam label="N =  " buttonText="Train for N Epochs" default="10" pythonCall="tran" onSubmit={(e: string) => generateTestGames(e)}/>
+
+      <Board saveBoard={(b: boardArray) => saveBoard(b)}/>
       <Table weights={weights} bias={bias}/>
       <div>
-        <ShowTrainingBoards title={"Training Dataset"} boards={train_data}/>
-        <ShowTrainingBoards title={"Test Dataset"} boards={test_data}/>
+        <ShowTrainingBoards title={"Training Dataset"} boards={trainData}/>
+        <ShowTrainingBoards title={"Test Dataset"} boards={testData}/>
       </div>
     </div>
   
